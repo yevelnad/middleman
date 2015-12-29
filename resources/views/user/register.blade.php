7b0588ec0ab56c7f07a5622592ee5e8817146998
yevelnad <yevelnad@gmail.com>
2015-12-28 21:43:23 -0800

    adding register

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -4,37 +4,29 @@
 Regiter here!
 @endsection
 @section('css')
-<link rel="stylesheet" href="{{asset('css/form.css')}}" media="screen" title="no title" charset="utf-8">
+<link rel="stylesheet" href="{{asset('css/register.css')}}" media="screen" title="no title" charset="utf-8">
 @endsection
 @section('content')
-<div class="col-sm-6 register">
-  <div class="col-sm-12 betrusted"><h1><p class="indent">Fast</p> and <p class="indent">Simple</p> Registration</h1></div>
-  <div class="col-sm-12"><h1><p class="earn">Earn</h1></div>
-  <div class="col-sm-12"><h1>for every</h1></div>
-  <div class="col-sm-12"><h1><p class="earn">Transaction</p></h1></div>
-</div>
-<div class="col-sm-6">
-    <form class="register" action="/user/login" method="post">
-
-      <div class="form-group">
-
-        <input type="text" name="name" value="" class="register" placeholder="username">
+  <div class="col-sm-12 register">
+    <div class="col-sm-7 register-text">
+      <div class="col-sm-12">
+        <div class="col-sm-6"><p class="register-content"><span class="glyphicon glyphicon-ok"> </span> Sign Up </p></div>
+        <div class="col-sm-1"></div>
+        <div class="col-sm-5"><p class="register-content"><span class="glyphicon glyphicon-ok"> </span> Trust </p></div>
+        <div class="col-sm-12">
+          <p class="register-content"><span class="glyphicon glyphicon-thumbs-up"> </span> Earn </p><p class="white"> by becoming a middleman.</p>
+          <p class="register-content"><span class="glyphicon glyphicon-hand-right"> </span>
+        </div>
       </div>
 
-      <div class="form-group">
-
-        <input type="text" name="email" value="" class="register" placeholder="email">
-      </div>
-
-      <div class="fomr-group">
-
-        <input type="password" name="name" value="" class="register" placeholder="password">
-      </div>
-        <input type="hidden" name="_token" value="{{csrf_token()}}">
-        <br />
-        <input type="submit" name="login" value="login" class="btn">
-
-    </form>
-
-</div>
+    </div>
+    <div class="col-sm-5 register-form">
+      <form class="" action="index.html" method="post">
+        <input type="text" name="name" value="" placeholder="username">
+        <input type="email" name="email" value="" placeholder="email">
+        <input type="password" name="password" value="" placeholder="password">
+        <input type="submit" name="register" value="register" class="button">
+      </form>
+    </div>
+  </div>
 @endsection
